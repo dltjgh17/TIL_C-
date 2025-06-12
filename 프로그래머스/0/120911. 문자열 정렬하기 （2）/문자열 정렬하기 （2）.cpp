c9b1bb95811fe5480dc7e51381340f92248c2f6a
dltjgh17 <dltkgh17@naver.com>
2025-06-12 13:11:07 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+
+    for (int i=0; i<my_string.length(); i++){
+        if (isupper(my_string[i])){
+            my_string[i] = tolower(my_string[i]);
+        }
+    }
+    sort(my_string.begin(), my_string.end());
+    return my_string;
+}
