f208dd875ed2685e8fcd74155cb29d5d1f73fbb1
dltjgh17 <dltkgh17@naver.com>
2025-09-08 09:44:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n=num;
+    while (n != 1 && answer < 500){
+        if (n%2==0){
+            n = n /2;
+        }
+        else{
+            n = n*3+1;
+        }
+        answer++;
+    }
+    return (n == 1) ? answer : -1;
+}
